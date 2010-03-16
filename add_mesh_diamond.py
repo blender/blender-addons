7db0e4eed602ed55ac129ec3912505b0d846e908
Campbell Barton <ideasman42@gmail.com>
2010-03-16 19:35:18 +0000

    pep8 cleanup

--- add_mesh_diamond.py
+++ add_mesh_diamond.py
@@ -11,7 +11,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
@@ -21,8 +21,6 @@
 # ***** END GPL LICENCE BLOCK *****
 
 import bpy
-import Mathutils
-from math import pi
 
 bl_addon_info = {
     'name': 'Add Mesh: Diamond',
@@ -33,130 +31,105 @@ bl_addon_info = {
     'url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/Scripts/Add_Diamond',
     'category': 'Add Mesh'}
 
-"""
-Name: 'Add_Mesh_Diamond'
-Blender: 250
-"""
-
-"""
-Name: 'Diamond'
-Blender: 250
-Group: 'AddMesh'
-Tip: 'Add Diamond Object...'
-__author__ = ["Four Mad Men", "FourMadMen.com"]
-__version__ = '2.00'
-__url__ = [
-	"Script, http://www.fourmadmen.com/blender/scripts/AddMesh/diamond/add_mesh_diamond.py", 
-	"Script Index, http://www.fourmadmen.com/blender/scripts/index.html", 
-	"Author Site , http://www.fourmadmen.com"]
-email__=["bwiki {at} fourmadmen {dot} com"]
-
-Usage:
-
-* Launch from Add Mesh menu
-
-* Modify parameters as desired or keep defaults
-
-"""
-
 
 def add_diamond(segments, girdle_radius, table_radius, crown_height, pavillion_height):
-	Vector = Mathutils.Vector
-	Quaternion = Mathutils.Quaternion
-	
-	PI_2 = pi * 2
-	z_axis = (0, 0, -1)
+    from Mathutils import Vector, Quaternion
+    from math import pi
 
-	verts = []
-	faces = []
-	
-	tot_verts = segments * 2 + 2
-	
-	height = crown_height + pavillion_height
-	half_height = height * .5
-	
-	verts.extend( Vector(0, 0, -half_height) )
-	verts.extend( Vector(0, 0, half_height) )
-	
-	i = 2
-	for index in range(segments):
-		quat = Quaternion(z_axis, (index / segments) * PI_2)
-		
-		angle = PI_2 * index / segments
-		
-		vec = Vector(table_radius, 0, -half_height) * quat
-		verts.extend([vec.x, vec.y, vec.z])
-		it1 = i
-		i+=1
-		
-		vec = Vector(girdle_radius, 0, half_height-pavillion_height) * quat
-		verts.extend([vec.x, vec.y, vec.z])
-		ib1 = i
-		i+=1
-		
-		if i>4:
-			faces.extend( [0, it1, it1-2, 0] )
-			faces.extend( [it1, ib1, ib1-2, it1-2] )
-			faces.extend( [1, ib1-2, ib1, 1] )
-	
-	faces.extend( [0, 2, it1, 0] )
-	faces.extend( [it1, 2, 3, ib1] )
-	faces.extend( [1, ib1, 3, 1] )
-	
-	return verts, faces
+    PI_2 = pi * 2
+    z_axis = (0.0, 0.0, -1.0)
+
+    verts = []
+    faces = []
+    
+    tot_verts = segments * 2 + 2
+
+    height = crown_height + pavillion_height
+    half_height = height * 0.5
+
+    verts.extend( Vector(0.0, 0.0, -half_height) )
+    verts.extend( Vector(0.0, 0.0, half_height) )
+
+    i = 2
+    for index in range(segments):
+        quat = Quaternion(z_axis, (index / segments) * PI_2)
+
+        angle = PI_2 * index / segments
+
+        vec = Vector(table_radius, 0, -half_height) * quat
+        verts.extend([vec.x, vec.y, vec.z])
+        it1 = i
+        i += 1
+
+        vec = Vector(girdle_radius, 0, half_height-pavillion_height) * quat
+        verts.extend([vec.x, vec.y, vec.z])
+        ib1 = i
+        i += 1
+
+        if i > 4:
+            faces.extend([0, it1, it1 - 2, 0])
+            faces.extend([it1, ib1, ib1 - 2, it1 - 2])
+            faces.extend([1, ib1 - 2, ib1, 1])
+
+    faces.extend([0, 2, it1, 0])
+    faces.extend([it1, 2, 3, ib1])
+    faces.extend([1, ib1, 3, 1])
+
+    return verts, faces
 
 from bpy.props import IntProperty, FloatProperty
 
+
 class AddDiamond(bpy.types.Operator):
-	'''Add a diamond mesh.'''
-	bl_idname = "mesh.diamond_add"
-	bl_label = "Add Diamond"
-	bl_options = {'REGISTER', 'UNDO'}
+    '''Add a diamond mesh.'''
+    bl_idname = "mesh.diamond_add"
+    bl_label = "Add Diamond"
+    bl_options = {'REGISTER', 'UNDO'}
 
-	segments = IntProperty(name="Segments",
-		description="Number of segments for the diamond",
-		default=32, min=3, max=256)
-	girdle_radius = FloatProperty(name="Girdle Radius",
-		description="Girdle radius of the diamond",
-		default=1.0, min=0.01, max=100.0)
-	table_radius = FloatProperty(name="Table Radius",
-		description="Girdle radius of the diamond",
-		default=0.8, min=0.01, max=100.0)
-	crown_height = FloatProperty(name="Crown Height",
-		description="Crown height of the diamond",
-		default=0.25, min=0.01, max=100.0)
-	pavillion_height = FloatProperty(name="Pavillion Height",
-		description="pavillion height of the diamond",
-		default=1.0, min=0.01, max=100.0)
+    segments = IntProperty(name="Segments",
+        description="Number of segments for the diamond",
+        default=32, min=3, max=256)
+    girdle_radius = FloatProperty(name="Girdle Radius",
+        description="Girdle radius of the diamond",
+        default=1.0, min=0.01, max=100.0)
+    table_radius = FloatProperty(name="Table Radius",
+        description="Girdle radius of the diamond",
+        default=0.8, min=0.01, max=100.0)
+    crown_height = FloatProperty(name="Crown Height",
+        description="Crown height of the diamond",
+        default=0.25, min=0.01, max=100.0)
+    pavillion_height = FloatProperty(name="Pavillion Height",
+        description="pavillion height of the diamond",
+        default=1.0, min=0.01, max=100.0)
 
-	def execute(self, context):
-		verts_loc, faces = add_diamond(self.properties.segments,
-			self.properties.girdle_radius,
-			self.properties.table_radius,
-			self.properties.crown_height,
-			self.properties.pavillion_height)
+    def execute(self, context):
+        verts_loc, faces = add_diamond(self.properties.segments,
+            self.properties.girdle_radius,
+            self.properties.table_radius,
+            self.properties.crown_height,
+            self.properties.pavillion_height)
 
-		mesh = bpy.data.meshes.new("Diamond")
-		
-		mesh.add_geometry(int(len(verts_loc) / 3), 0, int(len(faces) / 4))
-		mesh.verts.foreach_set("co", verts_loc)
-		mesh.faces.foreach_set("verts_raw", faces)
-		scene = context.scene
+        mesh = bpy.data.meshes.new("Diamond")
+        
+        mesh.add_geometry(int(len(verts_loc) / 3), 0, int(len(faces) / 4))
+        mesh.verts.foreach_set("co", verts_loc)
+        mesh.faces.foreach_set("verts_raw", faces)
+        scene = context.scene
 
-		# ugh
-		for ob in scene.objects:
-			ob.selected = False
+        # ugh
+        for ob in scene.objects:
+            ob.selected = False
 
-		mesh.update()
-		ob_new = bpy.data.objects.new("Diamond", mesh)
-		ob_new.data = mesh
-		scene.objects.link(ob_new)
-		scene.objects.active = ob_new
-		ob_new.selected = True
-		
-		ob_new.location = tuple(context.scene.cursor_location)
-		
-		return {'FINISHED'}
+        mesh.update()
+        ob_new = bpy.data.objects.new("Diamond", mesh)
+        ob_new.data = mesh
+        scene.objects.link(ob_new)
+        scene.objects.active = ob_new
+        ob_new.selected = True
+        
+        ob_new.location = tuple(context.scene.cursor_location)
+        
+        return {'FINISHED'}
 
 
 # Register the operator
@@ -174,8 +147,8 @@ def register():
 def unregister():
     bpy.types.unregister(AddDiamond)
     bpy.types.INFO_MT_mesh_add.remove(menu_func)
-	
-	    # Remove "Diamond" menu from the "Add Mesh" menu.
+
+    # Remove "Diamond" menu from the "Add Mesh" menu.
     #space_info.INFO_MT_mesh_add.remove(menu_func)
 
 if __name__ == "__main__":
