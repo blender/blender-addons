13e71191f23b0a567d30fac98a4fe7b93f655ca9
Martin Buerbaum <martin.buerbaum@gmx.at>
2010-04-15 11:02:36 +0000

    * Version 2.1 * Changed object/mesh creation code to use generic functions. * Also modified vertex/faces creation to be more readable. * Added recall data. * Fixed some formatting/comments

--- add_mesh_diamond.py
+++ add_mesh_diamond.py
@@ -21,65 +21,213 @@
 # ***** END GPL LICENCE BLOCK *****
 
 import bpy
+from mathutils import Vector, Quaternion
+from math import *
+from bpy.props import *
 
 bl_addon_info = {
     'name': 'Add Mesh: Diamond',
     'author': 'fourmadmen',
-    'version': '2.0',
+    'version': '2.1',
     'blender': (2, 5, 3),
     'location': 'View3D > Add > Mesh ',
     'description': 'Adds a mesh Diamond to the Add Mesh menu',
     'url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/' \
-	    'Scripts/Add_Mesh/Add_Diamond',
+        'Scripts/Add_Mesh/Add_Diamond',
     'category': 'Add Mesh'}
 
 
-def add_diamond(segments, girdle_radius, table_radius, crown_height, pavillion_height):
-    from mathutils import Vector, Quaternion
-    from math import pi
+# Stores the values of a list of properties and the
+# operator id in a property group ('recall_op') inside the object.
+# Could (in theory) be used for non-objects.
+# Note: Replaces any existing property group with the same name!
+# ob ... Object to store the properties in.
+# op ... The operator that should be used.
+# op_args ... A dictionary with valid Blender
+#             properties (operator arguments/parameters).
+def store_recall_properties(ob, op, op_args):
+    if ob and op and op_args:
+        recall_properties = {}
 
-    PI_2 = pi * 2
+        # Add the operator identifier and op parameters to the properties.
+        recall_properties['op'] = op.bl_idname
+        recall_properties['args'] = op_args
+
+        # Store new recall properties.
+        ob['recall'] = recall_properties
+
+
+# Apply view rotation to objects if "Align To" for
+# new objects was set to "VIEW" in the User Preference.
+def apply_object_align(context, ob):
+    obj_align = bpy.context.user_preferences.edit.object_align
+
+    if (context.space_data.type == 'VIEW_3D'
+        and obj_align == 'VIEW'):
+            view3d = context.space_data
+            region = view3d.region_3d
+            viewMatrix = region.view_matrix
+            rot = viewMatrix.rotation_part()
+            ob.rotation_euler = rot.invert().to_euler()
+
+
+# Create a new mesh (object) from verts/edges/faces.
+# verts/edges/faces ... List of vertices/edges/faces for the
+#                       new mesh (as used in from_pydata).
+# name ... Name of the new mesh (& object).
+# edit ... Replace existing mesh data.
+# Note: Using "edit" will destroy/delete existing mesh data.
+def create_mesh_object(context, verts, edges, faces, name, edit):
+    scene = context.scene
+    obj_act = scene.objects.active
+
+    # Can't edit anything, unless we have an active obj.
+    if edit and not obj_act:
+        return None
+
+    # Create new mesh
+    mesh = bpy.data.meshes.new(name)
+
+    # Make a mesh from a list of verts/edges/faces.
+    mesh.from_pydata(verts, edges, faces)
+
+    # Update mesh geometry after adding stuff.
+    mesh.update()
+
+    # Deselect all objects.
+    bpy.ops.object.select_all(action='DESELECT')
+
+    if edit:
+        # Replace geometry of existing object
+
+        # Use the active obj and select it.
+        ob_new = obj_act
+        ob_new.selected = True
+
+        if obj_act.mode == 'OBJECT':
+            # Get existing mesh datablock.
+            old_mesh = ob_new.data
+
+            # Set object data to nothing
+            ob_new.data = None
+
+            # Clear users of existing mesh datablock.
+            old_mesh.user_clear()
+
+            # Remove old mesh datablock if no users are left.
+            if (old_mesh.users == 0):
+                bpy.data.meshes.remove(old_mesh)
+
+            # Assign new mesh datablock.
+            ob_new.data = mesh
+
+    else:
+        # Create new object
+        ob_new = bpy.data.objects.new(name, mesh)
+
+        # Link new object to the given scene and select it.
+        scene.objects.link(ob_new)
+        ob_new.selected = True
+
+        # Place the object at the 3D cursor location.
+        ob_new.location = scene.cursor_location
+
+        apply_object_align(context, ob_new)
+
+    if obj_act and obj_act.mode == 'EDIT':
+        if not edit:
+            # We are in EditMode, switch to ObjectMode.
+            bpy.ops.object.mode_set(mode='OBJECT')
+
+            # Select the active object as well.
+            obj_act.selected = True
+
+            # Apply location of new object.
+            scene.update()
+
+            # Join new object into the active.
+            bpy.ops.object.join()
+
+            # Switching back to EditMode.
+            bpy.ops.object.mode_set(mode='EDIT')
+
+            ob_new = obj_act
+
+    else:
+        # We are in ObjectMode.
+        # Make the new object the active one.
+        scene.objects.active = ob_new
+
+    return ob_new
+
+
+def add_diamond(segments, girdle_radius, table_radius,
+    crown_height, pavillion_height):
+
+    PI_2 = pi * 2.0
     z_axis = (0.0, 0.0, -1.0)
 
     verts = []
     faces = []
 
-    tot_verts = segments * 2 + 2
-
     height = crown_height + pavillion_height
     half_height = height * 0.5
 
-    verts.extend(Vector(0.0, 0.0, -half_height))
-    verts.extend(Vector(0.0, 0.0, half_height))
+    # Vertex counter
+    vert_cnt = 0
 
-    i = 2
+    # Tip
+    verts.append((0.0, 0.0, -half_height))
+    vert_tip = vert_cnt
+    vert_cnt += 1
+
+    # Middle vertex of the flat side
+    verts.append((0.0, 0.0, half_height))
+    vert_flat = vert_cnt
+    vert_cnt += 1
+
+    vert1_prev = None
+    vert2_prev = None
     for index in range(segments):
         quat = Quaternion(z_axis, (index / segments) * PI_2)
 
         angle = PI_2 * index / segments
 
-        vec = Vector(table_radius, 0, -half_height) * quat
-        verts.extend([vec.x, vec.y, vec.z])
-        it1 = i
-        i += 1
+        vec = Vector(table_radius, 0.0, -half_height) * quat
+        verts.append((vec.x, vec.y, vec.z))
+        vert1 = vert_cnt
+        vert_cnt += 1
 
-        vec = Vector(girdle_radius, 0, half_height - pavillion_height) * quat
-        verts.extend([vec.x, vec.y, vec.z])
-        ib1 = i
-        i += 1
+        vec = Vector(girdle_radius, 0.0, half_height - pavillion_height) * quat
+        verts.append((vec.x, vec.y, vec.z))
+        vert2 = vert_cnt
+        vert_cnt += 1
 
-        if i > 4:
-            faces.extend([0, it1, it1 - 2, 0])
-            faces.extend([it1, ib1, ib1 - 2, it1 - 2])
-            faces.extend([1, ib1 - 2, ib1, 1])
+        if index == 0:
+            # Remember vertex indices for next iteration.
+            vert1_first = vert1
+            vert2_first = vert2
 
-    faces.extend([0, 2, it1, 0])
-    faces.extend([it1, 2, 3, ib1])
-    faces.extend([1, ib1, 3, 1])
+        else:
+            # Tip face
+            faces.append([vert_tip, vert1_prev, vert1])
+            # Side face
+            faces.append([vert2, vert1, vert1_prev, vert2_prev])
+            # Flat face
+            faces.append([vert_flat, vert2, vert2_prev])
+
+        vert1_prev = vert1
+        vert2_prev = vert2
+
+    # Create the last faces between first+last vertices
+    # Tip face
+    faces.append([vert_tip, vert1, vert1_first])
+    # Side face
+    faces.append([vert2_first, vert1_first, vert1, vert2])
+    # Flat face
+    faces.append([vert_flat, vert2_first, vert2])
 
     return verts, faces
-
-from bpy.props import IntProperty, FloatProperty
 
 
 class AddDiamond(bpy.types.Operator):
@@ -88,6 +236,11 @@ class AddDiamond(bpy.types.Operator):
     bl_label = "Add Diamond"
     bl_options = {'REGISTER', 'UNDO'}
 
+    # edit - Whether to add or update.
+    edit = BoolProperty(name="",
+        description="",
+        default=False,
+        options={'HIDDEN'})
     segments = IntProperty(name="Segments",
         description="Number of segments for the diamond",
         default=32, min=3, max=256)
@@ -105,56 +258,46 @@ class AddDiamond(bpy.types.Operator):
         default=1.0, min=0.01, max=100.0)
 
     def execute(self, context):
-        verts_loc, faces = add_diamond(self.properties.segments,
-            self.properties.girdle_radius,
-            self.properties.table_radius,
-            self.properties.crown_height,
-            self.properties.pavillion_height)
+        props = self.properties
+        verts, faces = add_diamond(props.segments,
+            props.girdle_radius,
+            props.table_radius,
+            props.crown_height,
+            props.pavillion_height)
 
-        mesh = bpy.data.meshes.new("Diamond")
+        obj = create_mesh_object(context, verts, [], faces,
+            "Diamond", props.edit)
 
-        mesh.add_geometry(int(len(verts_loc) / 3), 0, int(len(faces) / 4))
-        mesh.verts.foreach_set("co", verts_loc)
-        mesh.faces.foreach_set("verts_raw", faces)
-        scene = context.scene
-
-        # ugh
-        for ob in scene.objects:
-            ob.selected = False
-
-        mesh.update()
-        ob_new = bpy.data.objects.new("Diamond", mesh)
-        ob_new.data = mesh
-        scene.objects.link(ob_new)
-        scene.objects.active = ob_new
-        ob_new.selected = True
-
-        ob_new.location = tuple(context.scene.cursor_location)
+         # Store 'recall' properties in the object.
+        recall_args_list = {
+            "edit": True,
+            "segments": props.segments,
+            "girdle_radius": props.girdle_radius,
+            "table_radius": props.table_radius,
+            "crown_height": props.crown_height,
+            "pavillion_height": props.pavillion_height}
+        store_recall_properties(obj, self, recall_args_list)
 
         return {'FINISHED'}
 
 
 # Register the operator
-# Add to a menu, reuse an icon used elsewhere that happens to have fitting name
-# unfortunately, the icon shown is the one I expected from looking at the
-# blenderbuttons file from the release/datafiles directory
-
 menu_func = (lambda self, context: self.layout.operator(AddDiamond.bl_idname,
-                                        text="Add Diamond", icon='PLUGIN'))
+                                        text="Diamond", icon='PLUGIN'))
 
 
 def register():
     bpy.types.register(AddDiamond)
+
+    # Add "Diamond" entry to the "Add Mesh" menu.
     bpy.types.INFO_MT_mesh_add.append(menu_func)
 
 
 def unregister():
     bpy.types.unregister(AddDiamond)
-    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
-    # Remove "Diamond" menu from the "Add Mesh" menu.
-    #space_info.INFO_MT_mesh_add.remove(menu_func)
+    # Remove "Diamond" entry from the "Add Mesh" menu.
+    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
 if __name__ == "__main__":
     register()
-
