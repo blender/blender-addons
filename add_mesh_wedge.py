e046b6d0d9a748a9b465766305852161171a5f4e
Martin Buerbaum <martin.buerbaum@gmx.at>
2010-04-20 21:57:08 +0000

    * Version 1.2 of the "Wedge" script. * Script now supports "recall" properties (Edit Object Parameters) * Renamed size parameters to "Size X", "Size Y" and "Size Z" * Cleanup of code & comments.

--- add_mesh_wedge.py
+++ add_mesh_wedge.py
@@ -23,11 +23,12 @@
 bl_addon_info = {
     'name': 'Add Mesh: Wedge',
     'author': 'fourmadmen',
-    'version': '1.1',
+    'version': '1.2',
     'blender': (2, 5, 3),
     'location': 'View3D > Add > Mesh ',
     'description': 'Adds a mesh Wedge to the Add Mesh menu',
-    'url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/Scripts/Add_Wedge',
+    'url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/' \
+        'Scripts/Add_Wedge',
     'category': 'Add Mesh'}
 
 
@@ -51,101 +52,304 @@ Usage:
 
 
 import bpy
-import mathutils
+from bpy.props import *
+from mathutils import *
 
 
-def add_wedge( wedge_width, wedge_height, wedge_depth):
+# Stores the values of a list of properties and the
+# operator id in a property group ('recall_op') inside the object.
+# Could (in theory) be used for non-objects.
+# Note: Replaces any existing property group with the same name!
+# ob ... Object to store the properties in.
+# op ... The operator that should be used.
+# op_args ... A dictionary with valid Blender
+#             properties (operator arguments/parameters).
+def store_recall_properties(ob, op, op_args):
+    if ob and op and op_args:
+        recall_properties = {}
 
-    Vector = mathutils.Vector
+        # Add the operator identifier and op parameters to the properties.
+        recall_properties['op'] = op.bl_idname
+        recall_properties['args'] = op_args
 
+        # Store new recall properties.
+        ob['recall'] = recall_properties
+
+
+# Apply view rotation to objects if "Align To" for
+# new objects was set to "VIEW" in the User Preference.
+def apply_object_align(context, ob):
+    obj_align = bpy.context.user_preferences.edit.object_align
+
+    if (context.space_data.type == 'VIEW_3D'
+        and obj_align == 'VIEW'):
+            view3d = context.space_data
+            region = view3d.region_3d
+            viewMatrix = region.view_matrix
+            rot = viewMatrix.rotation_part()
+            ob.rotation_euler = rot.invert().to_euler()
+
+
+# Create a new mesh (object) from verts/edges/faces.
+# verts/edges/faces ... List of vertices/edges/faces for the
+#                       new mesh (as used in from_pydata).
+# name ... Name of the new mesh (& object).
+# edit ... Replace existing mesh data.
+# Note: Using "edit" will destroy/delete existing mesh data.
+def create_mesh_object(context, verts, edges, faces, name, edit):
+    scene = context.scene
+    obj_act = scene.objects.active
+
+    # Can't edit anything, unless we have an active obj.
+    if edit and not obj_act:
+        return None
+
+    # Create new mesh
+    mesh = bpy.data.meshes.new(name)
+
+    # Make a mesh from a list of verts/edges/faces.
+    mesh.from_pydata(verts, edges, faces)
+
+    # Update mesh geometry after adding stuff.
+    mesh.update()
+
+    # Deselect all objects.
+    bpy.ops.object.select_all(action='DESELECT')
+
+    if edit:
+        # Replace geometry of existing object
+
+        # Use the active obj and select it.
+        ob_new = obj_act
+        ob_new.selected = True
+
+        if obj_act.mode == 'OBJECT':
+            # Get existing mesh datablock.
+            old_mesh = ob_new.data
+
+            # Set object data to nothing
+            ob_new.data = None
+
+            # Clear users of existing mesh datablock.
+            old_mesh.user_clear()
+
+            # Remove old mesh datablock if no users are left.
+            if (old_mesh.users == 0):
+                bpy.data.meshes.remove(old_mesh)
+
+            # Assign new mesh datablock.
+            ob_new.data = mesh
+
+    else:
+        # Create new object
+        ob_new = bpy.data.objects.new(name, mesh)
+
+        # Link new object to the given scene and select it.
+        scene.objects.link(ob_new)
+        ob_new.selected = True
+
+        # Place the object at the 3D cursor location.
+        ob_new.location = scene.cursor_location
+
+        apply_object_align(context, ob_new)
+
+    if obj_act and obj_act.mode == 'EDIT':
+        if not edit:
+            # We are in EditMode, switch to ObjectMode.
+            bpy.ops.object.mode_set(mode='OBJECT')
+
+            # Select the active object as well.
+            obj_act.selected = True
+
+            # Apply location of new object.
+            scene.update()
+
+            # Join new object into the active.
+            bpy.ops.object.join()
+
+            # Switching back to EditMode.
+            bpy.ops.object.mode_set(mode='EDIT')
+
+            ob_new = obj_act
+
+    else:
+        # We are in ObjectMode.
+        # Make the new object the active one.
+        scene.objects.active = ob_new
+
+    return ob_new
+
+
+# A very simple "bridge" tool.
+# Connects two equally long vertex rows with faces.
+# Returns a list of the new faces (list of  lists)
+#
+# vertIdx1 ... First vertex list (list of vertex indices).
+# vertIdx2 ... Second vertex list (list of vertex indices).
+# closed ... Creates a loop (first & last are closed).
+# flipped ... Invert the normal of the face(s).
+#
+# Note: You can set vertIdx1 to a single vertex index to create
+#       a fan/star of faces.
+# Note: If both vertex idx list are the same length they have
+#       to have at least 2 vertices.
+def createFaces(vertIdx1, vertIdx2, closed=False, flipped=False):
+    faces = []
+
+    if not vertIdx1 or not vertIdx2:
+        return None
+
+    if len(vertIdx1) < 2 and len(vertIdx2) < 2:
+        return None
+
+    fan = False
+    if (len(vertIdx1) != len(vertIdx2)):
+        if (len(vertIdx1) == 1 and len(vertIdx2) > 1):
+            fan = True
+        else:
+            return None
+
+    total = len(vertIdx2)
+
+    if closed:
+        # Bridge the start with the end.
+        if flipped:
+            face = [
+                vertIdx1[0],
+                vertIdx2[0],
+                vertIdx2[total - 1]]
+            if not fan:
+                face.append(vertIdx1[total - 1])
+            faces.append(face)
+
+        else:
+            face = [vertIdx2[0], vertIdx1[0]]
+            if not fan:
+                face.append(vertIdx1[total - 1])
+            face.append(vertIdx2[total - 1])
+            faces.append(face)
+
+    # Bridge the rest of the faces.
+    for num in range(total - 1):
+        if flipped:
+            if fan:
+                face = [vertIdx2[num], vertIdx1[0], vertIdx2[num + 1]]
+            else:
+                face = [vertIdx2[num], vertIdx1[num],
+                    vertIdx1[num + 1], vertIdx2[num + 1]]
+            faces.append(face)
+        else:
+            if fan:
+                face = [vertIdx1[0], vertIdx2[num], vertIdx2[num + 1]]
+            else:
+                face = [vertIdx1[num], vertIdx2[num],
+                    vertIdx2[num + 1], vertIdx1[num + 1]]
+            faces.append(face)
+
+    return faces
+
+
+# @todo Align wedge aling axis?
+def add_wedge(size_x, size_y, size_z):
     verts = []
     faces = []
 
-    half_width = wedge_width * .5
-    half_height = wedge_height * .5
-    half_depth = wedge_depth * .5
-  
-    verts.extend( Vector(-half_width, -half_height, half_depth) )
-    verts.extend( Vector(-half_width, -half_height, -half_depth) )
+    size_x /= 2.0
+    size_y /= 2.0
+    size_z /= 2.0
 
-    verts.extend( Vector(half_width, -half_height, half_depth) )
-    verts.extend( Vector(half_width, -half_height, -half_depth) )
+    vIdx_top = []
+    vIdx_bot = []
 
-    verts.extend( Vector(-half_width, half_height, half_depth) )
-    verts.extend( Vector(-half_width, half_height, -half_depth) )
+    vIdx_top.append(len(verts))
+    verts.append(Vector(-size_x, -size_y, size_z))
+    vIdx_bot.append(len(verts))
+    verts.append(Vector(-size_x, -size_y, -size_z))
 
-    faces.extend( [0, 2, 4, 0] )
-    faces.extend( [1, 3, 5, 1] )
-    faces.extend( [0, 1, 3, 2] )
-    faces.extend( [0, 4, 5, 1] )
-    faces.extend( [2, 3, 5, 4] )
+    vIdx_top.append(len(verts))
+    verts.append(Vector(size_x, -size_y, size_z))
+    vIdx_bot.append(len(verts))
+    verts.append(Vector(size_x, -size_y, -size_z))
+
+    vIdx_top.append(len(verts))
+    verts.append(Vector(-size_x, size_y, size_z))
+    vIdx_bot.append(len(verts))
+    verts.append(Vector(-size_x, size_y, -size_z))
+
+    faces.append(vIdx_top)
+    faces.append(vIdx_bot)
+    faces_outside = createFaces(vIdx_top, vIdx_bot, closed=True)
+    faces.extend(faces_outside)
 
     return verts, faces
 
-from bpy.props import FloatProperty
 
 class AddWedge(bpy.types.Operator):
     '''Add a wedge mesh.'''
-    bl_idname = "mesh.wedge_add"
+    bl_idname = "mesh.primitive_wedge_add"
     bl_label = "Add Wedge"
     bl_options = {'REGISTER', 'UNDO'}
 
-    wedge_width = FloatProperty(name="Width",
-        description="Width of Wedge",
-        default=2.00, min=0.01, max=100.00)
-    wedge_height = FloatProperty(name="Height",
-        description="Height of Wedge",
-        default=2.00, min=0.01, max=100.00)
-    wedge_depth = FloatProperty(name="Depth",
-        description="Depth of Wedge",
-        default=2.00, min=0.01, max=100.00)
+    # edit - Whether to add or update.
+    edit = BoolProperty(name="",
+        description="",
+        default=False,
+        options={'HIDDEN'})
+    size_x = FloatProperty(name="Size X",
+        description="Size along the X axis",
+        min=0.01,
+        max=9999.0,
+        default=2.0)
+    size_y = FloatProperty(name="Size Y",
+        description="Size along the Y axis",
+        min=0.01,
+        max=9999.0,
+        default=2.0)
+    size_z = FloatProperty(name="Size Z",
+        description="Size along the Z axis",
+        min=0.01,
+        max=9999.0,
+        default=2.00)
 
     def execute(self, context):
+        props = self.properties
 
-        verts_loc, faces = add_wedge(self.properties.wedge_width, self.properties.wedge_height, self.properties.wedge_depth)
+        verts, faces = add_wedge(
+            props.size_x,
+            props.size_y,
+            props.size_z)
 
-        mesh = bpy.data.meshes.new("Wedge")
+        obj = create_mesh_object(context, verts, [], faces, "Wedge",
+            props.edit)
 
-        mesh.add_geometry(int(len(verts_loc) / 3), 0, int(len(faces) / 4))
-        mesh.verts.foreach_set("co", verts_loc)
-        mesh.faces.foreach_set("verts_raw", faces)
-        mesh.faces.foreach_set("smooth", [False] * len(mesh.faces))
-
-        scene = context.scene
-
-        # ugh
-        for ob in scene.objects:
-            ob.selected = False
-
-        mesh.update()
-        ob_new = bpy.data.objects.new("Wedge", mesh)
-        ob_new.data = mesh
-        scene.objects.link(ob_new)
-        scene.objects.active = ob_new
-        ob_new.selected = True
-
-        ob_new.location = tuple(context.scene.cursor_location)
+        # Store 'recall' properties in the object.
+        recall_args_list = {
+            "edit": True,
+            "size_x": props.size_x,
+            "size_y": props.size_y,
+            "size_z": props.size_z}
+        store_recall_properties(obj, self, recall_args_list)
 
         return {'FINISHED'}
 
 
 # Register the operator
-# Add to a menu, reuse an icon used elsewhere that happens to have fitting name
-# unfortunately, the icon shown is the one I expected from looking at the
-# blenderbuttons file from the release/datafiles directory
+menu_func = (lambda self, context: self.layout.operator(AddWedge.bl_idname,
+    text="Add Wedge", icon='PLUGIN'))
 
-menu_func = (lambda self, context: self.layout.operator(AddWedge.bl_idname, text="Add Wedge", icon='PLUGIN'))
 
 def register():
     bpy.types.register(AddWedge)
+
+    # Add "Wedge" menu to the "Add Mesh" menu.
     bpy.types.INFO_MT_mesh_add.append(menu_func)
+
 
 def unregister():
     bpy.types.unregister(AddWedge)
-    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
-# Remove "Wedge" menu from the "Add Mesh" menu.
-#space_info.INFO_MT_mesh_add.remove(menu_func)
+    # Remove "Wedge" menu from the "Add Mesh" menu.
+    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
 if __name__ == "__main__":
     register()
