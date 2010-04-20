f90772245546859ac7321c6f1da6e0edacdd12dc
Martin Buerbaum <martin.buerbaum@gmx.at>
2010-04-20 13:06:10 +0000

    * Version 2.1 of the "star" script * Now supports recall proeprties (Edit Object Parameters) * Cleaned up the verts/faces creation process.

--- add_mesh_star.py
+++ add_mesh_star.py
@@ -23,23 +23,216 @@
 bl_addon_info = {
     'name': 'Add Mesh: Star',
     'author': 'fourmadmen',
-    'version': '2.0',
+    'version': '2.1',
     'blender': (2, 5, 3),
     'location': 'View3D > Add > Mesh ',
     'description': 'Adds a mesh Star to the Add Mesh menu',
     'url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/' \
-	    'Scripts/Add_Mesh/Add_Star',
+        'Scripts/Add_Mesh/Add_Star',
     'category': 'Add Mesh'}
 
 
 import bpy
 import mathutils
+from mathutils import Vector, Quaternion
+from math import pi
+from bpy.props import IntProperty, FloatProperty, BoolProperty
 
 
-def add_star(points, outer_radius, inner_radius, depth):
-    from mathutils import Vector, Quaternion
-    from math import pi
+# Stores the values of a list of properties and the
+# operator id in a property group ('recall_op') inside the object.
+# Could (in theory) be used for non-objects.
+# Note: Replaces any existing property group with the same name!
+# ob ... Object to store the properties in.
+# op ... The operator that should be used.
+# op_args ... A dictionary with valid Blender
+#             properties (operator arguments/parameters).
+def store_recall_properties(ob, op, op_args):
+    if ob and op and op_args:
+        recall_properties = {}
 
+        # Add the operator identifier and op parameters to the properties.
+        recall_properties['op'] = op.bl_idname
+        recall_properties['args'] = op_args
+
+        # Store new recall properties.
+        ob['recall'] = recall_properties
+
+
+# Apply view rotation to objects if "Align To" for
+# new objects was set to "VIEW" in the User Preference.
+def apply_object_align(context, ob):
+    obj_align = bpy.context.user_preferences.edit.object_align
+
+    if (context.space_data.type == 'VIEW_3D'
+        and obj_align == 'VIEW'):
+            view3d = context.space_data
+            region = view3d.region_3d
+            viewMatrix = region.view_matrix
+            rot = viewMatrix.rotation_part()
+            ob.rotation_euler = rot.invert().to_euler()
+
+
+# Create a new mesh (object) from verts/edges/faces.
+# verts/edges/faces ... List of vertices/edges/faces for the
+#                       new mesh (as used in from_pydata).
+# name ... Name of the new mesh (& object).
+# edit ... Replace existing mesh data.
+# Note: Using "edit" will destroy/delete existing mesh data.
+def create_mesh_object(context, verts, edges, faces, name, edit):
+    scene = context.scene
+    obj_act = scene.objects.active
+
+    # Can't edit anything, unless we have an active obj.
+    if edit and not obj_act:
+        return None
+
+    # Create new mesh
+    mesh = bpy.data.meshes.new(name)
+
+    # Make a mesh from a list of verts/edges/faces.
+    mesh.from_pydata(verts, edges, faces)
+
+    # Update mesh geometry after adding stuff.
+    mesh.update()
+
+    # Deselect all objects.
+    bpy.ops.object.select_all(action='DESELECT')
+
+    if edit:
+        # Replace geometry of existing object
+
+        # Use the active obj and select it.
+        ob_new = obj_act
+        ob_new.selected = True
+
+        if obj_act.mode == 'OBJECT':
+            # Get existing mesh datablock.
+            old_mesh = ob_new.data
+
+            # Set object data to nothing
+            ob_new.data = None
+
+            # Clear users of existing mesh datablock.
+            old_mesh.user_clear()
+
+            # Remove old mesh datablock if no users are left.
+            if (old_mesh.users == 0):
+                bpy.data.meshes.remove(old_mesh)
+
+            # Assign new mesh datablock.
+            ob_new.data = mesh
+
+    else:
+        # Create new object
+        ob_new = bpy.data.objects.new(name, mesh)
+
+        # Link new object to the given scene and select it.
+        scene.objects.link(ob_new)
+        ob_new.selected = True
+
+        # Place the object at the 3D cursor location.
+        ob_new.location = scene.cursor_location
+
+        apply_object_align(context, ob_new)
+
+    if obj_act and obj_act.mode == 'EDIT':
+        if not edit:
+            # We are in EditMode, switch to ObjectMode.
+            bpy.ops.object.mode_set(mode='OBJECT')
+
+            # Select the active object as well.
+            obj_act.selected = True
+
+            # Apply location of new object.
+            scene.update()
+
+            # Join new object into the active.
+            bpy.ops.object.join()
+
+            # Switching back to EditMode.
+            bpy.ops.object.mode_set(mode='EDIT')
+
+            ob_new = obj_act
+
+    else:
+        # We are in ObjectMode.
+        # Make the new object the active one.
+        scene.objects.active = ob_new
+
+    return ob_new
+
+
+# A very simple "bridge" tool.
+# Connects two equally long vertex rows with faces.
+# Returns a list of the new faces (list of  lists)
+#
+# vertIdx1 ... First vertex list (list of vertex indices).
+# vertIdx2 ... Second vertex list (list of vertex indices).
+# closed ... Creates a loop (first & last are closed).
+# flipped ... Invert the normal of the face(s).
+#
+# Note: You can set vertIdx1 to a single vertex index to create
+#       a fan/star of faces.
+# Note: If both vertex idx list are the same length they have
+#       to have at least 2 vertices.
+def createFaces(vertIdx1, vertIdx2, closed=False, flipped=False):
+    faces = []
+
+    if not vertIdx1 or not vertIdx2:
+        return None
+
+    if len(vertIdx1) < 2 and len(vertIdx2) < 2:
+        return None
+
+    fan = False
+    if (len(vertIdx1) != len(vertIdx2)):
+        if (len(vertIdx1) == 1 and len(vertIdx2) > 1):
+            fan = True
+        else:
+            return None
+
+    total = len(vertIdx2)
+
+    if closed:
+        # Bridge the start with the end.
+        if flipped:
+            face = [
+                vertIdx1[0],
+                vertIdx2[0],
+                vertIdx2[total - 1]]
+            if not fan:
+                face.append(vertIdx1[total - 1])
+            faces.append(face)
+
+        else:
+            face = [vertIdx2[0], vertIdx1[0]]
+            if not fan:
+                face.append(vertIdx1[total - 1])
+            face.append(vertIdx2[total - 1])
+            faces.append(face)
+
+    # Bridge the rest of the faces.
+    for num in range(total - 1):
+        if flipped:
+            if fan:
+                face = [vertIdx2[num], vertIdx1[0], vertIdx2[num + 1]]
+            else:
+                face = [vertIdx2[num], vertIdx1[num],
+                    vertIdx1[num + 1], vertIdx2[num + 1]]
+            faces.append(face)
+        else:
+            if fan:
+                face = [vertIdx1[0], vertIdx2[num], vertIdx2[num + 1]]
+            else:
+                face = [vertIdx1[num], vertIdx2[num],
+                    vertIdx2[num + 1], vertIdx1[num + 1]]
+            faces.append(face)
+
+    return faces
+
+
+def add_star(points, outer_radius, inner_radius, height):
     PI_2 = pi * 2
     z_axis = (0, 0, 1)
 
@@ -47,44 +240,48 @@ def add_star(points, outer_radius, inner_radius, depth):
     faces = []
 
     segments = points * 2
-    tot_verts = segments * 2 + 2
 
-    half_height = depth * 0.5
+    half_height = height / 2.0
 
-    verts.extend(Vector(0.0, 0.0, -half_height))
-    verts.extend(Vector(0.0, 0.0, half_height))
+    vert_idx_top = len(verts)
+    verts.append(Vector(0.0, 0.0, half_height))
 
-    i = 2
-    alt_idx = 0
+    vert_idx_bottom = len(verts)
+    verts.append(Vector(0.0, 0.0, -half_height))
+
+    edgeloop_top = []
+    edgeloop_bottom = []
+
     for index in range(segments):
         quat = Quaternion(z_axis, (index / segments) * PI_2)
 
-        radius = alt_idx and inner_radius or outer_radius
+        if index % 2:
+            # Uneven
+            radius = outer_radius
+        else:
+            # Even
+            radius = inner_radius
 
-        vec = Vector(radius, 0, -half_height) * quat
-        verts.extend([vec.x, vec.y, vec.z])
-        it1 = i
-        i += 1
-
+        edgeloop_top.append(len(verts))
         vec = Vector(radius, 0, half_height) * quat
-        verts.extend([vec.x, vec.y, vec.z])
-        ib1 = i
-        i += 1
+        verts.append(vec)
 
-        if i > 4:
-            faces.extend([0, it1 - 2, it1, 0])
-            faces.extend([it1, it1 - 2, ib1 - 2, ib1])
-            faces.extend([1, ib1, ib1 - 2, 1])
+        edgeloop_bottom.append(len(verts))
+        vec = Vector(radius, 0, -half_height) * quat
+        verts.append(vec)
 
-        alt_idx = 1 - alt_idx
 
-    faces.extend([0, it1, 2, 0])
-    faces.extend([2, it1, ib1, 3])
-    faces.extend([1, 3, ib1, 1])
+
+    faces_top = createFaces([vert_idx_top], edgeloop_top, closed=True)
+    faces_outside = createFaces(edgeloop_top, edgeloop_bottom, closed=True)
+    faces_bottom = createFaces([vert_idx_bottom], edgeloop_bottom,
+        flipped=True, closed=True)
+
+    faces.extend(faces_top)
+    faces.extend(faces_outside)
+    faces.extend(faces_bottom)
 
     return verts, faces
-
-from bpy.props import IntProperty, FloatProperty
 
 
 class AddStar(bpy.types.Operator):
@@ -93,53 +290,56 @@ class AddStar(bpy.types.Operator):
     bl_label = "Add Star"
     bl_options = {'REGISTER', 'UNDO'}
 
+    # edit - Whether to add or update.
+    edit = BoolProperty(name="",
+        description="",
+        default=False,
+        options={'HIDDEN'})
     points = IntProperty(name="Points",
         description="Number of points for the star",
-        default=5, min=2, max=256)
+        min=2,
+        max=256,
+        default=5)
     outer_radius = FloatProperty(name="Outer Radius",
         description="Outer radius of the star",
-        default=1.0, min=0.01, max=100.0)
+        min=0.01,
+        max=9999.0,
+        default=1.0)
     innter_radius = FloatProperty(name="Inner Radius",
         description="Inner radius of the star",
-        default=0.5, min=0.01, max=100.0)
-    depth = FloatProperty(name="Depth",
-        description="Depth of the star",
-        default=0.5, min=0.01, max=100.0)
+        min=0.01,
+        max=9999.0,
+        default=0.5)
+    height = FloatProperty(name="Height",
+        description="Height of the star",
+        min=0.01,
+        max=9999.0,
+        default=0.5)
 
     def execute(self, context):
-        verts_loc, faces = add_star(self.properties.points,
-            self.properties.outer_radius,
-            self.properties.innter_radius,
-            self.properties.depth)
+        props = self.properties
 
-        mesh = bpy.data.meshes.new("Star")
+        verts, faces = add_star(
+            props.points,
+            props.outer_radius,
+            props.innter_radius,
+            props.height)
 
-        mesh.add_geometry(int(len(verts_loc) / 3), 0, int(len(faces) / 4))
-        mesh.verts.foreach_set("co", verts_loc)
-        mesh.faces.foreach_set("verts_raw", faces)
+        obj = create_mesh_object(context, verts, [], faces, "Star",
+            props.edit)
 
-        scene = context.scene
-
-        # ugh
-        for ob in scene.objects:
-            ob.selected = False
-
-        mesh.update()
-
-        ob_new = bpy.data.objects.new("Star", mesh)
-        ob_new.data = mesh
-        scene.objects.link(ob_new)
-        scene.objects.active = ob_new
-        ob_new.selected = True
-
-        ob_new.location = tuple(context.scene.cursor_location)
+        # Store 'recall' properties in the object.
+        recall_args_list = {
+            "edit": True,
+            "points": props.points,
+            "outer_radius": props.outer_radius,
+            "innter_radius": props.innter_radius,
+            "height": props.height}
+        store_recall_properties(obj, self, recall_args_list)
 
         return {'FINISHED'}
 
 # Register the operator
-# Add to a menu, reuse an icon used elsewhere that happens to have fitting name
-# unfortunately, the icon shown is the one I expected from looking at the
-# blenderbuttons file from the release/datafiles directory
 
 menu_func = (lambda self, context: self.layout.operator(AddStar.bl_idname,
                                         text="Star", icon='PLUGIN'))
@@ -147,15 +347,16 @@ menu_func = (lambda self, context: self.layout.operator(AddStar.bl_idname,
 
 def register():
     bpy.types.register(AddStar)
+
+    # Add "Star" menu to the "Add Mesh" menu.
     bpy.types.INFO_MT_mesh_add.append(menu_func)
 
 
 def unregister():
     bpy.types.unregister(AddStar)
-    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
     # Remove "Star" menu from the "Add Mesh" menu.
-    #space_info.INFO_MT_mesh_add.remove(menu_func)
+    bpy.types.INFO_MT_mesh_add.remove(menu_func)
 
 if __name__ == "__main__":
     register()
