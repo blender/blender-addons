273d45951dbce5c15374177e4e190bdd78f4b3cd
Brendon Murphy <meta.androcto1@gmail.com>
2010-06-16 08:49:09 +0000

    return to 3D View Menu with note that script returns errors.

--- space_view3d_panel_measure.py
+++ space_view3d_panel_measure.py
@@ -28,12 +28,12 @@ bl_addon_info = {
     'version': '0.7',
     'blender': (2, 5, 3),
     'location': 'View3D > Properties > Measure',
-    'description': 'Measure distances between objects',
+    'description': '* note: script returns errors but works.',
     'wiki_url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/' \
         'Scripts/3D_interaction/Panel_Measure',
     'tracker_url': 'https://projects.blender.org/tracker/index.php?'\
         'func=detail&aid=21445&group_id=153&atid=469',
-    'category': 'Broken'}
+    'category': '3D View'}
 
 # More links:
 # http://gitorious.org/blender-scripts/blender-measure-panel-script
