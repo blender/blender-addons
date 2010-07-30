50579a2cf8f4bdfbe625f1e617c2bedf891ddaf8
Fabian Fricke <frigi.f@googlemail.com>
2010-07-30 18:37:36 +0000

    update for api change

--- object_fracture/fracture_setup.py
+++ object_fracture/fracture_setup.py
@@ -35,7 +35,7 @@ def setupshards(context):
 
     tobeprocessed = []
     for ob in sce.objects:
-        if ob.selected:
+        if ob.select:
             tobeprocessed.append(ob)
 
     for ob in tobeprocessed:
