15a1f1794fc4b26d50df085507860d21e0e3fcae
Brendon Murphy <meta.androcto1@gmail.com>
2010-09-10 13:25:03 +0000

    updating revision numbers as I test. expect more. Thanks Mindrones for this cool feature.

--- space_view3d_align_tools.py
+++ space_view3d_align_tools.py
@@ -24,7 +24,7 @@ bl_addon_info = {
     "author": "Gabriel Beaudin (gabhead)",
     "version": (0,1),
     "blender": (2, 5, 3),
-    "api": 31667,
+    "api": 31854,
     "location": "Tool Shelf",
     "description": "Align selected objects to the active object",
     "warning": "",
