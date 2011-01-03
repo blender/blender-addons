f9913fcfd8f176ea2217d6cfb6fd35e88678afa1
Remigiusz Fiedler <migius@gmx.net>
2011-01-03 02:24:13 +0000

    updated tracker_url

--- io_import_scene_dxf.py
+++ io_import_scene_dxf.py
@@ -68,7 +68,7 @@ bl_addon_info = {
     'description': 'Import files in the Autocad DXF format (.dxf)',
     'warning': 'supporting only a sub-set of DXF specification',
     'wiki_url': 'http://wiki.blender.org/index.php/Extensions:2.5/Py/Scripts/Import-Export/DXF_Importer',
-    'tracker_url': 'https://projects.blender.org/tracker/index.php?func=detail&aid=23480&group_id=153&atid=468',
+    'tracker_url': 'https://projects.blender.org/tracker/index.php?func=detail&aid=23480&group_id=153&atid=469',
     'category': 'Import-Export'}
 
 __version__ = '.'.join([str(s) for s in bl_addon_info['version']])
