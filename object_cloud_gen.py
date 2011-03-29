39361da22f8e2e001bec1fbd26f669cd19bf9bdc
Brendon Murphy <meta.androcto1@gmail.com>
2011-03-29 00:49:00 +0000

    addons/object_cloud_gen.py added "warning": "Script returns errors" api r35853

--- object_cloud_gen.py
+++ object_cloud_gen.py
@@ -24,7 +24,7 @@ bl_info = {
     "api": 35622,
     "location": "View3D > Tool Shelf > Cloud Generator Panel",
     "description": "Creates Volumetric Clouds",
-    "warning": "",
+    "warning": "Script returns errors",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.5/Py/"\
         "Scripts/Object/Cloud_Gen",
     "tracker_url": "https://projects.blender.org/tracker/index.php?"\
