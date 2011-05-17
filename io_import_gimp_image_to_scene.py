50d154eb1ed421fbbe00deea9baa90f05e122f88
Daniel Salazar <zanqdo@gmail.com>
2011-05-17 02:50:56 +0000

    Update Gimp importer for API changes

--- io_import_gimp_image_to_scene.py
+++ io_import_gimp_image_to_scene.py
@@ -293,7 +293,8 @@ def main(File, Path, LayerViewers, MixerViewers, LayerOffset,\
         enter_editmode=False,\
         rotation=(0, 0, pi))
         
-        bpy.ops.object.rotation_apply()
+        bpy.ops.object.transform_apply(location=False, rotation=True, scale=False)
+
         
         Active = bpy.context.active_object
         
@@ -310,7 +311,7 @@ def main(File, Path, LayerViewers, MixerViewers, LayerOffset,\
             
         Active.dimensions = float(Coords[0])*LayerScale, float(Coords[1])*LayerScale, 0
         
-        bpy.ops.object.scale_apply()
+        bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
         
         bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
         
