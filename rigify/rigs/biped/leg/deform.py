01dd77b498f1e27dc856ad93e9165a599e4b6538
Campbell Barton <ideasman42@gmail.com>
2011-07-25 02:30:51 +0000

    use reverse vector multiplication order

--- rigify/rigs/biped/leg/deform.py
+++ rigify/rigs/biped/leg/deform.py
@@ -48,7 +48,7 @@ def align_roll(obj, bone1, bone2):
     rot_mat = Matrix.Rotation(angle, 3, axis)
 
     # Roll factor
-    x3 = x1 * rot_mat
+    x3 = rot_mat * x1
     dot = x2 * x3
     if dot > 1.0:
         dot = 1.0
@@ -60,7 +60,7 @@ def align_roll(obj, bone1, bone2):
     bone1_e.roll = roll
 
     # Check if we rolled in the right direction
-    x3 = bone1_e.x_axis * rot_mat
+    x3 = rot_mat * bone1_e.x_axis
     check = x2 * x3
 
     # If not, reverse
