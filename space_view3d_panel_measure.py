a90f3ac68158795efd17aa16229f865406c5e2e0
Brendon Murphy <meta.androcto1@gmail.com>
2011-09-08 13:49:27 +0000

    added warning to script & bug report on tracker page.

--- space_view3d_panel_measure.py
+++ space_view3d_panel_measure.py
@@ -24,7 +24,7 @@ bl_info = {
     "api": 35864,
     "location": "View3D > Properties > Measure Panel",
     "description": "Measure distances between objects",
-    "warning": "",
+    "warning": "Script needs repairs",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.5/Py/" \
         "Scripts/3D_interaction/Panel_Measure",
     "tracker_url": "https://projects.blender.org/tracker/index.php?" \
