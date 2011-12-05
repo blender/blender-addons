ed74bfe14d302ab15c843ef0397fce9aa4008717
Campbell Barton <ideasman42@gmail.com>
2011-12-05 23:05:49 +0000

    fix for error added when refactoring

--- io_scene_map/export_map.py
+++ io_scene_map/export_map.py
@@ -47,7 +47,7 @@ def face_uv_get(face):
 def face_material_get(face):
     me = face.id_data
     try:
-        return me.materials[f.material_index]
+        return me.materials[face.material_index]
     except:
         return None
 
@@ -114,7 +114,7 @@ def write_face2brush(file, face):
 
     image_text = PREF_NULL_TEX
 
-    material = face_material_get(f)
+    material = face_material_get(face)
 
     if material and material.game_settings.invisible:
         image_text = PREF_INVIS_TEX
