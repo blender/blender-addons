bd5ff24747c72373e6ea694040f9fd2a264d02ea
Brendon Murphy <meta.androcto1@gmail.com>
2012-02-23 13:20:28 +0000

    fix for remove doubles bmesh

--- add_mesh_ant_landscape.py
+++ add_mesh_ant_landscape.py
@@ -779,7 +779,7 @@ class landscape_add(bpy.types.Operator):
             # sphere, remove doubles
             if self.SphereMesh !=0:
                 bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.remove_doubles(limit=0.0001)
+                bpy.ops.mesh.remove_doubles(mergedist=0.0001)
                 bpy.ops.object.mode_set(mode='OBJECT')
 
             # Shade smooth
