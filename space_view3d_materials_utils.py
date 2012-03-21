5db0036867cbfb799c5f4188f994538fe4929626
Brendon Murphy <meta.androcto1@gmail.com>
2012-03-21 09:57:23 +0000

    adding buggy/broken warning

--- space_view3d_materials_utils.py
+++ space_view3d_materials_utils.py
@@ -29,7 +29,7 @@ bl_info = {
     "blender": (2, 6, 2),
     "location": "View3D > Q key",
     "description": "Menu of material tools (assign, select..)  in the 3D View",
-    "warning": "",
+    "warning": "Buggy, Broken in Cycles mode",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.5/Py/"
                 "Scripts/3D interaction/Materials Utils",
     "tracker_url": "https://projects.blender.org/tracker/index.php?"
