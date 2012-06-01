ef545f4a6397ff6ee84c66b096c80d1aac9fe9b5
Campbell Barton <ideasman42@gmail.com>
2012-06-01 21:29:57 +0000

    use a tuple rather then a dict()

--- modules/rna_wiki_reference.py
+++ modules/rna_wiki_reference.py
@@ -5,13 +5,14 @@
 
 url_manual_prefix = "http://wiki.blender.org/index.php/Doc:2.6/Manual/"
 
-# the key is infact a regex mapping '.*' means anything.
-url_manual_mapping = {
-    "bpy.types.ArmatureModifier.*": "Modifiers/Deform/Armature",
-    "bpy.types.SmoothModifier.*": "Modifiers/Deform/Smooth",
-    "bpy.types.SubsurfModifier.*":  "Modifiers/Generate/Subsurf",
+# - The first item is a regex mapping '.*' means anything.
+# - Expressions are evaluated top down (include catch-all expressions last).
+url_manual_mapping = (
+    ("bpy.types.ArmatureModifier.*", "Modifiers/Deform/Armature"),
+    ("bpy.types.SmoothModifier.*", "Modifiers/Deform/Smooth"),
+    ("bpy.types.SubsurfModifier.*", "Modifiers/Generate/Subsurf"),
     
-    "bpy.types.Material.diffuse.*":  "Materials/Properties/Diffuse_Shaders",
-}
+    ("bpy.types.Material.diffuse.*", "Materials/Properties/Diffuse_Shaders"),
+)
 
 # may have 'url_reference_mapping'... etc later
