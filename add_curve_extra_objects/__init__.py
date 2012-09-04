30ab42921b158414caab48b6c94a3a988be5ed78
Brendon Murphy <meta.androcto1@gmail.com>
2012-09-04 00:07:39 +0000

    update tracker url

--- add_curve_extra_objects/__init__.py
+++ add_curve_extra_objects/__init__.py
@@ -29,7 +29,7 @@ bl_info = {
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.6/Py/"\
         "Scripts/Curve/Curve_Objects",
     "tracker_url": "https://projects.blender.org/tracker/index.php?"\
-        "func=detail&aid=30824",
+        "func=detail&aid=32477",
     "category": "Add Curve"}
 
 
