cc8f5dce95bb41a54ba4f0393720bb82ed424052
Nathan Vegdahl <cessen@cessen.com>
2013-04-24 00:11:45 +0000

    Rigify bugfix: dev tools panel was displayed regardless of mode.
    Thanks to Sanc Tuary for the bug report! Bug #35040

--- rigify/ui.py
+++ rigify/ui.py
@@ -202,6 +202,10 @@ class VIEW3D_PT_tools_rigify_dev(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'TOOLS'
 
+    @classmethod
+    def poll(cls, context):
+        return context.mode == 'EDIT_ARMATURE' or context.mode == 'EDIT_MESH'
+
     def draw(self, context):
         obj = context.active_object
         if obj != None:
