3e19e7145fcf53d8331cf9c3e940e8aa866575d1
Bastien Montagne <montagne29@wanadoo.fr>
2014-05-07 19:09:03 +0200

    Fix T40069: PLY import vertex colour scaling.
    devide chars by 255, not 256...

--- io_mesh_ply/import_ply.py
+++ io_mesh_ply/import_ply.py
@@ -239,7 +239,7 @@ def load_ply_mesh(filepath, ply_name):
             if -1 in colindices:
                 colindices = None
             else:  # if not a float assume uchar
-                colmultiply = [1.0 if el.properties[i].numeric_type in {'f', 'd'} else (1.0 / 256.0) for i in colindices]
+                colmultiply = [1.0 if el.properties[i].numeric_type in {'f', 'd'} else (1.0 / 255.0) for i in colindices]
 
         elif el.name == b'face':
             findex = el.index(b'vertex_indices')
