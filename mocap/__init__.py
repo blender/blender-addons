0de4052432b34680d4441704b99ae2238eae25a1
Bastien Montagne <montagne29@wanadoo.fr>
2014-10-29 12:33:17 +0100

    Fix a minor UI issue when object has animdata but no action, raise Blender version, add addon version.

--- mocap/__init__.py
+++ mocap/__init__.py
@@ -21,7 +21,8 @@
 bl_info = {
     "name": "Motion Capture Tools",
     "author": "Benjy Cook",
-    "blender": (2, 62, 0),
+    "blender": (2, 72, 0),
+    "version": (1, 0, 1),
     "location": "Object UI > Mocap tools",
     "description": "Various tools for working with motion capture animation",
     "warning": "",
@@ -333,7 +334,7 @@ class MocapPanel(bpy.types.Panel):
                     mapRow.operator("mocap.savemapping", text='Save mapping')
                     mapRow.operator("mocap.loadmapping", text='Load mapping')
                     extraSettings = self.layout.box()
-                    if performer_obj.animation_data:
+                    if performer_obj.animation_data and performer_obj.animation_data.action:
                         extraSettings.prop(data=performer_obj.animation_data.action, property='name', text='Action Name')
                     extraSettings.prop(enduser_arm, "frameStep")
                     extraSettings.prop(enduser_arm, "advancedRetarget", text='Advanced Retarget')
