7f3f92f158f11979dcd145eb4283fb50c8043d10
meta-androcto <meta.androcto1@gmail.com>
2015-12-03 21:42:43 +1100

    Add warning: "may produce errors, fix in progress"

--- io_import_scene_unreal_psa_psk.py
+++ io_import_scene_unreal_psa_psk.py
@@ -23,7 +23,7 @@ bl_info = {
     "blender": (2, 64, 0),
     "location": "File > Import > Skeleton Mesh (.psk)/Animation Set (psa)",
     "description": "Import Skeleleton Mesh/Animation Data",
-    "warning": "",
+    "warning": "may produce errors, fix in progress",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:2.5/Py/"
                 "Scripts/Import-Export/Unreal_psk_psa",
     "category": "Import-Export",
