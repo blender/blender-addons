70a889052ee519b5a1f77edff569d76e104c6c7f
Campbell Barton <ideasman42@gmail.com>
2016-03-15 12:56:15 +1100

    Correct typo

--- uv_texture_atlas.py
+++ uv_texture_atlas.py
@@ -102,7 +102,7 @@ class TexAtl_Main(Panel):
         # Resolution and Unwrap types (only if Lightmap group is added)
         if context.scene.ms_lightmap_groups:
             group = scene.ms_lightmap_groups[scene.ms_lightmap_groups_index]
-            row.label(text="Resolutiom:")
+            row.label(text="Resolution:")
             row.prop(group, 'resolutionX', text='')
             row.prop(group, 'resolutionY', text='')
             row = self.layout.row()
