cb10969e467adee25fc8ec201a41d7d2fbebfcbf
Sybren A. Stüvel <sybren@stuvel.eu>
2017-06-19 15:11:46 +0200

    Fixed ant_landscape/test.py so that it can be imported
    This allows the `bl_load_py_modules.py` unit test to do its job.

--- ant_landscape/test.py
+++ ant_landscape/test.py
@@ -1,19 +1,21 @@
-from stats import Stats
 from numpy import *
 
-stats = Stats()
+if __name__ == '__main__':
+    # This only works when this script is loaded as main, or
+    # run directly from the ant_landscape directory.
+    from stats import Stats
 
-a = zeros(10000000)
-print(stats.time())
-print(stats.memory())
-a = sin(a)
-print(stats.time())
-print(stats.memory())
-a = cos(a)
-print(stats.time())
-print(stats.memory())
-a = cos(a)**2+sin(a)**2
-print(stats.time())
-print(stats.memory())
+    stats = Stats()
 
-
+    a = zeros(10000000)
+    print(stats.time())
+    print(stats.memory())
+    a = sin(a)
+    print(stats.time())
+    print(stats.memory())
+    a = cos(a)
+    print(stats.time())
+    print(stats.memory())
+    a = cos(a)**2+sin(a)**2
+    print(stats.time())
+    print(stats.memory())
