8a409b2561ee683ce7c24fb81dc0bb2228996a41
Campbell Barton <ideasman42@gmail.com>
2017-08-01 18:05:40 +1000

    Revert "Update lanuages"
    This reverts commit 15ce79c680dd63e5d54cc8ec28ad2c4d87a813ac.
    
    Turns out URL's stay the same

--- modules/rna_manual_reference.py
+++ modules/rna_manual_reference.py
@@ -21,8 +21,8 @@ LANG = {
     "ko_KR":        "ko",
     "pt_PT":        "pt",
     "pt_BR":        "pt",
-    "zh_CN":        "zh-hans",
-    "zh_TW":        "zh-hant",
+    "zh_CN":        "zh.cn",
+    "zh_TW":        "zh.tw",
 }.get(language)
 
 if LANG is not None:
