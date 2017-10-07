0ec1bacc4d1299700089ab11c6025d7be11354e1
lijenstina <lijenstina@gmail.com>
2017-10-07 20:03:45 +0200

    3D Viewport Pie Menus: Avoid empty Modes pie in some cases
    Bump version to 1.1.8
    
    Handle cases in pie_modes_menu when:
    - There is no active object
    - Object types that only have object mode available
    
    Add info messages covering them
    Minor cleanup (move bl_description before bl_options)

--- space_view3d_pie_menus/__init__.py
+++ space_view3d_pie_menus/__init__.py
@@ -33,7 +33,7 @@ from bpy.types import (
 bl_info = {
     "name": "3D Viewport Pie Menus",
     "author": "meta-androcto, pitiwazou, chromoly, italic",
-    "version": (1, 1, 7),
+    "version": (1, 1, 8),
     "blender": (2, 7, 7),
     "description": "Individual Pie Menu Activation List",
     "location": "Addons Preferences",
