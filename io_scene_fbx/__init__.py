4e84d14771b6c32e156469fee965e6db07e0ea84
Bastien Montagne <montagne29@wanadoo.fr>
2017-10-31 15:43:34 +0100

    Fix T53202: Complex armature with bone constraints, FBX export bug.
    Need more scene updates in FBX anim export code to ensure we do get sane
    poses...
    
    Conflicts:
    	io_scene_fbx/__init__.py

--- io_scene_fbx/__init__.py
+++ io_scene_fbx/__init__.py
@@ -21,8 +21,8 @@
 bl_info = {
     "name": "FBX format",
     "author": "Campbell Barton, Bastien Montagne, Jens Restemeier",
-    "version": (3, 7, 15),
-    "blender": (2, 79, 1),
+    "version": (3, 7, 16),
+    "blender": (2, 79, 0),
     "location": "File > Import-Export",
     "description": "FBX IO meshes, UV's, vertex colors, materials, textures, cameras, lamps and actions",
     "warning": "",
