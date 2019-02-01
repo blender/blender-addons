a292e6e25d23f00959ee3ab2050e11dc0a9febbb
Kalle-Samuli Riihikoski <haikalle@gmail.com>
2019-02-01 14:22:13 +0200

    Change category into 3DView.

--- io_coat3D/__init__.py
+++ io_coat3D/__init__.py
@@ -26,7 +26,7 @@ bl_info = {
     "warning": "",
     "wiki_url": "https://wiki.blender.org/index.php/Extensions:2.6/Py/"
                 "Scripts/Import-Export/3dcoat_applink",
-    "category": "Import-Export",
+    "category": "3D View",
 }
 
 
