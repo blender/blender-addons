69d8bf08be9c77c02b130c53495da1a56f9879e3
Brecht Van Lommel <brechtvanlommel@gmail.com>
2019-09-08 13:55:01 +0200

    Fix script_load_modules test failures in power sequencer

--- power_sequencer/scripts/BPSRender/setup.py
+++ power_sequencer/scripts/BPSRender/setup.py
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU General Public License along with Power Sequencer. If
 # not, see <https://www.gnu.org/licenses/>.
 #
-from setuptools import setup
-
 
 def readme():
     with open("README.rst") as f:
@@ -23,6 +21,8 @@ def readme():
 
 
 if __name__ == "__main__":
+    from setuptools import setup
+
     setup(
         name="bpsrender",
         version="0.1.40.post1",
