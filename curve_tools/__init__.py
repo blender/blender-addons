f4dabc29ff49a58ac8c4ebcd90d77d1709ec464a
meta-androcto <meta.androcto1@gmail.com>
2019-09-24 10:00:07 +1000

    curve tools: fix tab to Edit: T70017

--- curve_tools/__init__.py
+++ curve_tools/__init__.py
@@ -27,7 +27,7 @@ bl_info = {
     "author": "Mackraken",
     "version": (0, 3, 3),
     "blender": (2, 80, 0),
-    "location": "View3D > Tool Shelf > Addons Tab",
+    "location": "View3D > Tool Shelf > Edit Tab",
     "warning": "WIP",
     "wiki_url": "https://wiki.blender.org/index.php/Extensions:2.6/Py/"
                 "Scripts/Curve/Curve_Tools",
@@ -249,7 +249,7 @@ class VIEW3D_PT_CurvePanel(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_options = {'DEFAULT_CLOSED'}
-    bl_category = "Tools"
+    bl_category = "Edit"
 
     @classmethod
     def poll(cls, context):
@@ -486,7 +486,7 @@ class CurveAddonPreferences(AddonPreferences):
     category: StringProperty(
             name="Tab Category",
             description="Choose a name for the category of the panel",
-            default="CurveTools",
+            default="Edit",
             update=update_panel
             )
 
