b9b1814a4c26f73aae7f306c9ff2e21b7b7bdcee
Julien Duroure <julien.duroure@gmail.com>
2019-10-12 17:05:38 +0200

    glTF exporter: option to export only deformation bones

--- io_scene_gltf2/__init__.py
+++ io_scene_gltf2/__init__.py
@@ -15,7 +15,7 @@
 bl_info = {
     'name': 'glTF 2.0 format',
     'author': 'Julien Duroure, Norbert Nopper, Urs Hanselmann, Moritz Becher, Benjamin Schmithüsen, Jim Eckerlein, and many external contributors',
-    "version": (1, 0, 5),
+    "version": (1, 0, 6),
     'blender': (2, 81, 6),
     'location': 'File > Import-Export',
     'description': 'Import-Export as glTF 2.0',
@@ -257,6 +257,12 @@ class ExportGLTF2_Base:
         default=True
     )
 
+    export_def_bones: BoolProperty(
+        name='Export Deformation bones only',
+        description='Export Deformation bones only (and needed bones for hierarchy)',
+        default=False
+    )
+
     export_current_frame: BoolProperty(
         name='Use Current Frame',
         description='Export the scene in the current animation frame',
@@ -390,11 +396,16 @@ class ExportGLTF2_Base:
         if self.export_animations:
             export_settings['gltf_frame_range'] = self.export_frame_range
             export_settings['gltf_force_sampling'] = self.export_force_sampling
+            if self.export_force_sampling:
+                export_settings['gltf_def_bones'] = self.export_def_bones
+            else:
+                export_settings['gltf_def_bones'] = False
             export_settings['gltf_nla_strips'] = self.export_nla_strips
         else:
             export_settings['gltf_frame_range'] = False
             export_settings['gltf_move_keyframes'] = False
             export_settings['gltf_force_sampling'] = False
+            export_settings['gltf_def_bones'] = False
         export_settings['gltf_skins'] = self.export_skins
         if self.export_skins:
             export_settings['gltf_all_vertex_influences'] = self.export_all_influences
@@ -642,6 +653,10 @@ class GLTF_PT_export_animation_export(bpy.types.Panel):
         layout.prop(operator, 'export_frame_step')
         layout.prop(operator, 'export_force_sampling')
         layout.prop(operator, 'export_nla_strips')
+
+        row = layout.row()
+        row.active = operator.export_force_sampling
+        row.prop(operator, 'export_def_bones')
 
 
 class GLTF_PT_export_animation_shapekeys(bpy.types.Panel):
