1bd7fdc6ac0cbb8ff5dbd716c8f29881a4a47acf
Rune Morling <ermo.blender.org@spammesenseless.net>
2020-03-28 18:59:12 +0100

    PDT: Update version info

--- precision_drawing_tools/__init__.py
+++ precision_drawing_tools/__init__.py
@@ -29,8 +29,8 @@
 bl_info = {
     "name": "Precision Drawing Tools (PDT)",
     "author": "Alan Odom (Clockmender), Rune Morling (ermo)",
-    "version": (1, 2, 1),
-    "blender": (2, 80, 0),
+    "version": (1, 3, 0),
+    "blender": (2, 82, 0),
     "location": "View3D > UI > PDT",
     "description": "Precision Drawing Tools for Acccurate Modelling",
     "warning": "",
