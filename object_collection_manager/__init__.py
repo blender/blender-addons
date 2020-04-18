583a81397e1f45542c7264eb7f29c6253912e8d7
Ryan Inch <mythologylover75@gmail.com>
2020-04-18 02:43:46 -0400

    Collection Manager: Expander fix. Task T69577
    Fix expanded and expand history not getting updated
    on collection removal.

--- object_collection_manager/__init__.py
+++ object_collection_manager/__init__.py
@@ -22,12 +22,7 @@ bl_info = {
     "name": "Collection Manager",
     "description": "Manage collections and their objects",
     "author": "Ryan Inch",
-<<<<<<< HEAD
     "version": (2,7,11),
-=======
-    "version": (2,7,10),
->>>>>>> Collection Manager: Expander fixes. Task: T69577
-    "blender": (2, 80, 0),
     "location": "View3D - Object Mode (Shortcut - M)",
     "warning": '',  # used for warning icon and text in addons panel
     "doc_url": "{BLENDER_MANUAL_URL}/addons/interface/collection_manager.html",
