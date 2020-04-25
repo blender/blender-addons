0618a7636e543a507102bf4b425ab6f41d2c40bc
Ryan Inch <mythologylover75@gmail.com>
2020-04-25 04:19:33 -0400

    Collection Manager: Fix selection issues. Task: T69577
    Make treeview selection more stable and predictable.
    Fix not being able to select the row from the left side with
    a top level expander -- adjusts window sizing to account for this.

--- object_collection_manager/operators.py
+++ object_collection_manager/operators.py
@@ -76,13 +76,15 @@ class SetActiveCollection(Operator):
 
     def execute(self, context):
         if self.collection_index == -1:
-            cm = context.scene.collection_manager
-            cm.cm_list_index = -1
             layer_collection = context.view_layer.layer_collection
 
         else:
             laycol = layer_collections[self.collection_name]
             layer_collection = laycol["ptr"]
+
+            # set selection to this row
+            cm = context.scene.collection_manager
+            cm.cm_list_index = laycol["row_index"]
 
         context.view_layer.active_layer_collection = layer_collection
 
@@ -119,7 +121,6 @@ class ExpandAllOperator(Operator):
 
 
 class ExpandSublevelOperator(Operator):
-    ''''''
     bl_label = "Expand Sublevel Items"
     bl_description = (
         "  * Ctrl+LMB - Expand/Collapse all sublevels\n"
@@ -204,8 +205,7 @@ class ExpandSublevelOperator(Operator):
             expand_history["history"].clear()
 
 
-        # set selected row to the collection you're expanding/collapsing and update tree view
-        context.scene.collection_manager.cm_list_index = self.index
+        #update tree view
         update_property_group(context)
 
         return {'FINISHED'}
@@ -862,6 +862,7 @@ class CMRemoveCollectionOperator(Operator):
         laycol = layer_collections[self.collection_name]
         collection = laycol["ptr"].collection
         parent_collection = laycol["parent"]["ptr"].collection
+        selected_row_name = cm.cm_list_collection[cm.cm_list_index].name
 
 
         # shift all objects in this collection to the parent collection
@@ -889,9 +890,21 @@ class CMRemoveCollectionOperator(Operator):
         update_property_group(context)
 
 
-        if len(cm.cm_list_collection) == cm.cm_list_index:
-            cm.cm_list_index = len(cm.cm_list_collection) - 1
-            update_property_group(context)
+        # update selected row
+        laycol = layer_collections.get(selected_row_name, None)
+        if laycol:
+            cm.cm_list_index = laycol["row_index"]
+
+        elif len(cm.cm_list_collection) == cm.cm_list_index:
+            cm.cm_list_index -=  1
+
+            if cm.cm_list_index > -1:
+                name = cm.cm_list_collection[cm.cm_list_index].name
+                laycol = layer_collections[name]
+                while not laycol["visible"]:
+                    laycol = laycol["parent"]
+
+                cm.cm_list_index = laycol["row_index"]
 
 
         # update qcd
