426383bde9dd79431ba4bb2d21a2ac92803696f4
Bastien Montagne <bastien@blender.org>
2020-05-27 16:26:03 +0200

    Fix T67708: UDP3DSMAX custom properties need even more work...
    Because, why would you stick to a single separator between name and
    value, right? Where would be the fun in that?

--- io_scene_fbx/__init__.py
+++ io_scene_fbx/__init__.py
@@ -21,7 +21,7 @@
 bl_info = {
     "name": "FBX format",
     "author": "Campbell Barton, Bastien Montagne, Jens Restemeier",
-    "version": (4, 21, 1),
+    "version": (4, 21, 2),
     "blender": (2, 90, 3),
     "location": "File > Import-Export",
     "description": "FBX IO meshes, UV's, vertex colors, materials, textures, cameras, lamps and actions",
