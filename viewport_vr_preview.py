a2915bc279620340979387bfb67b356b4e462e8e
Peter Kim <pk15950@gmail.com>
2020-11-07 19:05:54 +0900

    VR: Add "show custom overlays" setting

--- viewport_vr_preview.py
+++ viewport_vr_preview.py
@@ -964,6 +964,7 @@ class VIEW3D_PT_vr_session_view(Panel):
         col.prop(session_settings, "show_floor", text="Floor")
         col.prop(session_settings, "show_annotation", text="Annotations")
         col.prop(session_settings, "show_controllers", text="Controllers")
+        col.prop(session_settings, "show_custom_overlays", text="Custom Overlays")
         col.prop(session_settings, "show_selection", text="Selection")
         col.prop(session_settings, "selection_eye", text="Selection Eye")
 
