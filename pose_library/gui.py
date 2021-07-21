4475cbd11a636382d57571e0f5dfeff1f90bd6b7
Julian Eisel <julian@blender.org>
2021-07-21 19:36:56 +0200

    Pose Library: Update variable name after change in RNA
    I just renamed this variable in DNA/RNA.

--- pose_library/gui.py
+++ pose_library/gui.py
@@ -60,7 +60,7 @@ class VIEW3D_PT_pose_library(Panel):
             activate_op_props, drag_op_props = layout.template_asset_view(
                 "pose_assets",
                 workspace,
-                "active_asset_library",
+                "asset_library",
                 wm,
                 "pose_assets",
                 workspace,
