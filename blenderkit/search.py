afb8eb2feffb17034eb3ba70fa0c0f1d489cf3bc
Vilem Duha <vilem.duha@gmail.com>
2021-11-19 16:18:17 +0100

    BlenderKit: update tips

--- blenderkit/search.py
+++ blenderkit/search.py
@@ -83,7 +83,7 @@ thumb_full_download_threads = queue.Queue()
 reports_queue = queue.Queue()
 all_thumbs_loaded = True
 
-rtips_string = """You can disable tips in the add-on preferences.
+rtips_string = """YYou can disable tips in the add-on preferences.
 Ratings help us distribute funds to creators.
 Creators also gain credits for free assets from subscribers.
 Click or drag model or material in scene to link/append 
@@ -97,6 +97,9 @@ Login to upload your own models, materials or brushes.
 Use 'A' key over the asset bar to search assets by the same author.
 Use semicolon - ; to hide or show the AssetBar.
 Support the authors by subscribing to Full plan.
+Use the W key over the asset bar to open the Author's webpage.
+Use the R key for fast rating of assets.
+Use the X key to delete the asset from your hard drive.
 """
 rtips = rtips_string.splitlines()
 
