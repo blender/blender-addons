38212cf0bdd9c1af3b3031f238230a006c03a8a0
Pablo Vazquez <pablo@blender.org>
2023-02-28 15:50:43 +0100

    Amaranth: Improve Open Node Source Image feature
    * Add operator to the Node and Context menus.
    * Tweak operator name and description to be more descriptive.
    * More robust poll, check if the node type is supported.

--- amaranth/__init__.py
+++ amaranth/__init__.py
@@ -72,7 +72,7 @@ from amaranth.misc import (
 bl_info = {
     "name": "Amaranth Toolset",
     "author": "Pablo Vazquez, Bassam Kurdali, Sergey Sharybin, Lukas Tönne, Cesar Saez, CansecoGPC",
-    "version": (1, 0, 19),
+    "version": (1, 0, 20),
     "blender": (3, 2, 0),
     "location": "Everywhere!",
     "description": "A collection of tools and settings to improve productivity",
