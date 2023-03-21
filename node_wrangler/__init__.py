blender/blender-addons

commit f592ca10596a2189fe36119bc1cb6f08341a8b1d

Fix #104438 : Lazy mix connects wrong sockets
Because the new mix nodes have different modes, the correct output
socket to connect is not just the first one, but the first enabled one.
There were already some checks in place for this, but apparently not yet
in all the needed places.

The current state of the code for the "Merge Nodes" operator is
terrible, and should probably be refactored in a future update. It is
becoming very hard to maintain.

Pull Request #104457

node_wrangler/__init__.py
@@ -2095,8 +2095,6 @@ class NWMergeNodes(Operator, NWBase):
             if tree_type == 'GEOMETRY':
                 if nodes_list is selected_math or nodes_list is selected_vector or nodes_list is selected_mix:
                     node_type = 'ShaderNode'
-                    if mode == 'MIX':
-                        mode = 'ADD'
                 else:
                     node_type = 'GeometryNode'
             if merge_position == 'CENTER':
@@ -2230,16 +2228,16 @@ class NWMergeNodes(Operator, NWBase):
             if len(nodes_list) == 2:
                 if not first_selected_output.links:
                     second_selected = nodes[nodes_list[1][0]]
-                    for ss_link in second_selected.outputs[0].links:
+                    for ss_link in get_first_enabled_output(second_selected).links:
                         # Prevent cyclic dependencies when nodes to be merged are linked to one another.
                         # Link only if "to_node" index not in invalid indexes list.
                         if not self.link_creates_cycle(ss_link, invalid_nodes):
-                            links.new(last_add.outputs[0], ss_link.to_socket)
+                            links.new(get_first_enabled_output(last_add), ss_link.to_socket)
             # add links from last_add to all links 'to_socket' of out links of first selected.
             for fs_link in first_selected_output.links:
                 # Link only if "to_node" index not in invalid indexes list.
                 if not self.link_creates_cycle(fs_link, invalid_nodes):
-                    links.new(last_add.outputs[0], fs_link.to_socket)
+                    links.new(get_first_enabled_output(last_add), fs_link.to_socket)
             # add link from "first" selected and "first" add node
             node_to = nodes[count_after - 1]
             links.new(first_selected_output, node_to.inputs[first])

commit edb61b0249ff5a0f7dc35e38171257bd3a025fc4

Fix #104480: Node Wrangler: Lazy Mix on float outputs in GN error
Trying to connect two float outputs with the Lazy Mix operator failed
because there is no MIX mode for math nodes, which are used to mix
float values. This was caused by f592ca10.

A better fix may still need to be found to get a more consistent
behavior.

node_wrangler/__init__.py
@@ -2095,6 +2095,8 @@ class NWMergeNodes(Operator, NWBase):
             if tree_type == 'GEOMETRY':
                 if nodes_list is selected_math or nodes_list is selected_vector or nodes_list is selected_mix:
                     node_type = 'ShaderNode'
+                    if mode == 'MIX':
+                        mode = 'ADD'
                 else:
                     node_type = 'GeometryNode'
             if merge_position == 'CENTER':