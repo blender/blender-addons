ffd044d4cac82b28475c1fb4db15365816b6d24c
Sebastian Sille <nrgsille@noreply.localhost>
2023-05-01 07:39:09 +0200

    Export_3ds: Fixed bitwise flag setting
    Fixed mat_map flag setting
    
    Signed-off-by: Sebastian Sille <nrgsille@noreply.localhost>

--- io_scene_3ds/export_3ds.py
+++ io_scene_3ds/export_3ds.py
@@ -572,23 +572,23 @@ def make_material_texture_chunk(chunk_id, texslots, pct):
         for link in texslot.socket_dst.links:
             socket = link.from_socket.identifier
 
-        ma_sub_mapflags = _3ds_chunk(MAP_TILING)
-        mapflag = 0
+        mat_sub_mapflags = _3ds_chunk(MAP_TILING)
+        mapflags = 0
 
         # no perfect mapping for mirror modes - 3DS only has uniform mirror w. repeat=2
         if tex.extension == 'EXTEND':  # decal flag
-            mapflag |= 0x1
+            mapflags |= 0x1
         # CLIP maps to 3DS' decal flag
         if tex.extension == 'CLIP':  # no wrap
-            mapflag |= 0x10
+            mapflags |= 0x10
 
         if socket == 'Alpha':
-            mapflag |= 0x40  # summed area sampling 0x20
+            mapflags |= 0x40  # summed area sampling 0x20
             if tex.socket_dst.identifier in {'Base Color', 'Specular'}: 
-                mapflag |= 0x80 if tex.image.colorspace_settings.name=='Non-Color' else 0x200  # RGB tint
+                mapflags |= 0x80 if tex.image.colorspace_settings.name=='Non-Color' else 0x200  # RGB tint
 
-        ma_sub_mapflags.add_variable("mapflags", _3ds_ushort(mapflag))
-        ma_sub.add_subchunk(ma_sub_mapflags)
+        mat_sub_mapflags.add_variable("mapflags", _3ds_ushort(mapflags))
+        mat_sub.add_subchunk(mat_sub_mapflags)
 
         mat_sub_texblur = _3ds_chunk(MAT_MAP_TEXBLUR)  # Based on observation this is usually 1.0
         mat_sub_texblur.add_variable("maptexblur", _3ds_float(1.0))
