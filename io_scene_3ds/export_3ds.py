00c06e45998fe62bddfeb61499378566d18603ba
Sebastian Sille <nrgsille@noreply.localhost>
2023-07-19 19:04:33 +0200

    Export_3ds: Advanced target calculation
    Some finetuning in z-axis calculation
    Rounded up the diagonal to avoid too large tilt angles

--- io_scene_3ds/export_3ds.py
+++ io_scene_3ds/export_3ds.py
@@ -1050,7 +1050,7 @@ def calc_target(posi, tilt=0.0, pan=0.0):
     adjacent = math.radians(90)
     turn = 0.0 if abs(pan) < adjacent else -0.0
     lean = 0.0 if abs(tilt) > adjacent else -0.0
-    diagonal = math.copysign(math.sqrt(pow(posi.x ,2) + pow(posi.y ,2)), posi.y)
+    diagonal = math.ceil(math.sqrt(pow(posi.x ,2) + pow(posi.y ,2)))
     target_x = math.copysign(posi.x + (posi.y * math.tan(pan)), pan)
     target_y = math.copysign(posi.y + (posi.x * math.tan(adjacent - pan)), turn)
     target_z = math.copysign(diagonal * math.tan(adjacent - tilt), lean)
