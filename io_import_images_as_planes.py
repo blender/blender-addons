ea90c3402505d9a178453d611450016e9426a178
Damien Picard <dam.pic@free.fr>
2023-10-03 18:08:18 +0200

    Import Images: bump minimum Blender version after API change

--- io_import_images_as_planes.py
+++ io_import_images_as_planes.py
@@ -6,7 +6,7 @@ bl_info = {
     "name": "Import Images as Planes",
     "author": "Florian Meyer (tstscr), mont29, matali, Ted Schundler (SpkyElctrc), mrbimax",
     "version": (3, 5, 0),
-    "blender": (2, 91, 0),
+    "blender": (4, 0, 0),
     "location": "File > Import > Images as Planes or Add > Image > Images as Planes",
     "description": "Imports images and creates planes with the appropriate aspect ratio. "
                    "The images are mapped to the planes.",
