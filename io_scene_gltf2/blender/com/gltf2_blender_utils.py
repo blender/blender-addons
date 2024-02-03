3f5af897086b23a5de15879b0f0f8414e6513e92
Julien Duroure <julien.duroure@gmail.com>
2024-02-03 12:31:50 +0100

    glTF: missed import in a previous commit

--- io_scene_gltf2/blender/com/gltf2_blender_utils.py
+++ io_scene_gltf2/blender/com/gltf2_blender_utils.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import numpy as np
+
 def fast_structured_np_unique(arr, *args, **kwargs):
     """
     np.unique optimized for structured arrays when a sorted result is not required.
