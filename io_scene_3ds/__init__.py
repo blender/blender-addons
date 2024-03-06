8d5af9be72aa8793295fbfd3764de85479fbe626
Sebastian Sille <nrgsille@noreply.localhost>
2024-03-06 11:44:01 +0100

    io_scene_3ds: Outcommented drag & drop support
    Commented out drag & drop because it got blocked in menu

--- io_scene_3ds/__init__.py
+++ io_scene_3ds/__init__.py
@@ -46,7 +46,7 @@ class Import3DS(bpy.types.Operator, ImportHelper):
 
     filename_ext = ".3ds"
     filter_glob: StringProperty(default="*.3ds", options={'HIDDEN'})
-    filepath: StringProperty(subtype='FILE_PATH', options={'SKIP_SAVE'})
+    # filepath: StringProperty(subtype='FILE_PATH', options={'SKIP_SAVE'})
 
     constrain_size: FloatProperty(
         name="Constrain Size",
@@ -105,14 +105,14 @@ class Import3DS(bpy.types.Operator, ImportHelper):
         default=False,
     )
 
-    @classmethod
-    def poll(cls, context):
-        return (context.area and context.area.type == "VIEW_3D")
+    # @classmethod
+    # def poll(cls, context):
+    #     return (context.area and context.area.type == "VIEW_3D")
 
     def execute(self, context):
         from . import import_3ds
-        if not self.filepath or not self.filepath.endswith(".3ds"):
-            return {'CANCELLED'}
+        # if not self.filepath or not self.filepath.endswith(".3ds"):
+        #     return {'CANCELLED'}
 
         keywords = self.as_keywords(ignore=("axis_forward",
                                             "axis_up",
@@ -126,11 +126,11 @@ class Import3DS(bpy.types.Operator, ImportHelper):
 
         return import_3ds.load(self, context, **keywords)
 
-    def invoke(self, context, event):
-        if self.filepath:
-            return self.execute(context)
-        context.window_manager.fileselect_add(self)
-        return {'RUNNING_MODAL'}
+    # def invoke(self, context, event):
+    #     if self.filepath:
+    #         return self.execute(context)
+    #     context.window_manager.fileselect_add(self)
+    #     return {'RUNNING_MODAL'}
 
     def draw(self, context):
         pass
